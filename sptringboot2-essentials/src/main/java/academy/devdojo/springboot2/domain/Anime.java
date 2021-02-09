2760453275fb3ea1730ab0312b5968a4af7887fc
Gabriel Fernella <GabrielFernella@github.com>
2021-02-09 16:03:15 -0300

    Created first get api

--- sptringboot2-essentials/src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ sptringboot2-essentials/src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -8,4 +8,12 @@ public class Anime {
     }
 
     public Anime(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
