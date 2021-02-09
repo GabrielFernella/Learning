0c427d9c455e84eb19600650229a7646c2fe8d22
Gabriel Fernella <GabrielFernella@github.com>
2021-02-09 15:17:09 -0300

    Initialized Spring boot

---
+++ sptringboot2-essentials/src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -0,0 +1,11 @@
+package academy.devdojo.springboot2.domain;
+
+public class Anime {
+    private String name;
+
+    public Anime(String name) {
+        this.name = name;
+    }
+
+    public Anime(){}
+}
---
+++ sptringboot2-essentials/src/main/java/academy/devdojo/springboot2/start/ApplicationStart.java
@@ -0,0 +1,9 @@
+package academy.devdojo.springboot2.start;
+
+import org.springframework.boot.SpringApplication;
+
+public class ApplicationStart {
+    public static void main(String[] args){
+        SpringApplication.run(ApplicationStart.class, args);
+    }
+}
