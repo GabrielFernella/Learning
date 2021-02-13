e3e19fdd83271c0d7071d6f16b965b11cc102672
GabrielDEV <gabrielfernella07@hotmail.com>
2021-02-13 09:51:21 -0300

    ensure signupController test

---
+++ NodeTDD-Mango/2_login/clean_node_api/src/presentations/controllers/signupController.spec.ts
@@ -0,0 +1,24 @@
+import { SignUpController } from './signup'
+
+describe('SignUpController', () => {
+  test('Should return 400 if no name is provider', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpReponse = sut.handle(httpRequest)
+    expect(httpReponse.sttatusCode).toBe(400)
+  })
+})
+
+/*
+describe('', () => {
+  test('', () => {
+
+  })
+})
+*/
