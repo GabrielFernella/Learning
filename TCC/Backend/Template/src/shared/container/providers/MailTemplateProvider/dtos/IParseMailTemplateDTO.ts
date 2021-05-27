b657312a93467b233618f70dc1425f6ec6038254
GabrielDEV <gabrielfernella07@hotmail.com>
2021-05-27 19:50:41 -0300

    update

---
+++ TCC/Backend/Template/src/shared/container/providers/MailTemplateProvider/dtos/IParseMailTemplateDTO.ts
@@ -0,0 +1,9 @@
+// Interfaces de chaves, para inserir várias variáveis
+interface ITemplateVariables {
+  [key: string]: string | number;
+}
+
+export default interface IParseMailTemplateDTO {
+  file: string;
+  variables: ITemplateVariables;
+}
